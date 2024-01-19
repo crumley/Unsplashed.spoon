1a64c86f5d0232992d09974dde0e1ac229e01a6a
Ryan Crumley <ryan.crumley@shopify.com>
2024-01-18 20:25:19 -0600

    Get all pages of a collection

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ m.homepage = "https://github.com/Hammerspoon/Spoons"
 m.logger = logger.new('Unsplash', 'debug')
 
 -- Settings
-m.clientId = nil 
+m.clientId = nil
 m.monitorSpaces = true
 m.lastBackgroundFile = nil
 m.collections = {}
@@ -32,42 +32,42 @@ m.downloadPath = os.getenv("HOME") .. "/.Trash/"
 m.settingsKeyPrefix = m.name
 
 function m:init()
-  m.logger.d('init')
-  m.lastBackgroundFile = hs.settings.get(m.settingsKeyPrefix .. ".lastBackgroundFile")
+    m.logger.d('init')
+    m.lastBackgroundFile = hs.settings.get(m.settingsKeyPrefix .. ".lastBackgroundFile")
 end
 
 function m:start()
-  m.logger.d('start')
+    m.logger.d('start')
 
-  if m.monitorSpaces then
-    local w = hs.spaces.watcher.new(function(s)
-      m:_onSpaceChanged()
-    end)
-    w.start(w)
-  end
+    if m.monitorSpaces then
+        local w = hs.spaces.watcher.new(function(s)
+            m:_onSpaceChanged()
+        end)
+        w.start(w)
+    end
 end
 
 local function createDownloadTaskAsync(url, cb)
     local outputPath = m.downloadPath .. hs.hash.SHA1(hs.timer.absoluteTime()) .. ".jpg"
-    
-    local function curlCallback(exitCode, stdout, stderr )
+
+    local function curlCallback(exitCode, stdout, stderr)
         if exitCode == 0 then
             -- success
-            cb( outputPath, nil )
+            cb(outputPath, nil)
         else
             -- failure
-            cb( nil, { stdout, stderr } )
+            cb(nil, { stdout, stderr })
         end
     end
 
     m.logger.d('createDownloadTaskAsync.started', url, outputPath)
-    local task = hs.task.new("/usr/bin/curl", curlCallback, {"-L", url, "-o", outputPath })
+    local task = hs.task.new("/usr/bin/curl", curlCallback, { "-L", url, "-o", outputPath })
     task:start()
 
     return task
 end
 
-function m:getCollectionPhotos(collectionId, cb) 
+function m:getCollectionPhotos(collectionId, cb)
     local photosEntry = m.collections[collectionId]
     if photosEntry ~= nil then
         if photosEntry.expireTime >= os.time() then
@@ -76,18 +76,70 @@ function m:getCollectionPhotos(collectionId, cb)
         end
     end
 
-    local collectionPhotosUrl = "https://api.unsplash.com/collections/".. collectionId .. "/photos?per_page=1000&client_id=" .. m.clientId
+    local function cacheCb(photos, err)
+        m.collections[collectionId] = {
+            photos = photos,
+            expireTime = os.time() + m.collectionsCacheDurationSeconds,
+        }
+
+        cb(photos, err)
+    end
+
+    m:_getCollectionPhotos(collectionId, cacheCb)
+end
+
+function m:_getCollectionPhotos(collectionId, cb)
+    local photos = {}
+    local lastPhotoCount = #photos
+
+    local batchSize = 3
+    local completedCount = 0
+    local dispatchedCount = 0
+
+    local function nextBatch(accFn)
+        local index = 0
+        local startCount = completedCount
+        dispatchedCount = dispatchedCount + batchSize
+        while index < batchSize do
+            m:getCollectionPhotosPage(collectionId, startCount + index, accFn)
+            index = index + 1
+        end
+    end
+
+    local function chunkCb(newPhotos, err)
+        completedCount = completedCount + 1
+
+        if err ~= nil then
+            cb(nil, err)
+        end
+
+        hs.fnutils.each(newPhotos, function(url)
+            table.insert(photos, url)
+        end)
+
+        if completedCount >= dispatchedCount then
+            if lastPhotoCount == #photos then
+                return cb(photos, nil)
+            end
+
+            lastPhotoCount = #photos
+            nextBatch(chunkCb)
+        end
+    end
+
+    nextBatch(chunkCb)
+end
+
+function m:getCollectionPhotosPage(collectionId, pageIndex, cb)
+    local collectionPhotosUrl = "https://api.unsplash.com/collections/" ..
+        collectionId .. "/photos?page=" .. pageIndex .. "&per_page=30&client_id=" .. m.clientId
     hs.http.asyncGet(collectionPhotosUrl, {}, function(status, body, headers)
+        m.logger.d('getCollectionPhotosPage.resp', collectionPhotosUrl, status)
         if status == 200 then
             local response = hs.json.decode(body)
             local photos = hs.fnutils.imap(response, function(item)
-                return item['urls']['raw']    
+                return item['urls']['raw']
             end)
-
-            m.collections[collectionId] = {
-                photos = photos,
-                expireTime = os.time() + m.collectionsCacheDurationSeconds,
-            }
 
             cb(photos, nil)
         else
@@ -101,19 +153,19 @@ function m:setRandomDesktopPhotoFromCollection(collectionId)
     m:getCollectionPhotos(collectionId, function(photos)
         local index = math.random(1, #photos)
         m.logger.df('setRandomDesktopPhotoFromCollection.random-photo %i/%i', index, #photos, photos[index])
-        createDownloadTaskAsync( photos[index], function(file, err)
+        createDownloadTaskAsync(photos[index], function(file, err)
             if err ~= nil then
                 m.logger.e('Error downloading image', err)
                 return
             end
-            
+
             m.logger.d('setRandomDesktopPhotoFromCollection.setting-background', file)
             m:_setBackground(file)
         end)
     end)
 end
 
-function m:_setBackground(file) 
+function m:_setBackground(file)
     m.lastBackgroundFile = file
     hs.settings.set(m.settingsKeyPrefix .. ".lastBackgroundFile", file)
     -- hs.screen.mainScreen():desktopImageURL("file://" .. file)
@@ -132,7 +184,7 @@ function m:setRandomDesktopPhoto()
     local height = string.format("%0.f", sd.h * sd.scale)
     local url = "https://source.unsplash.com/random/" .. width .. "x" .. height
 
-    createDownloadTaskAsync( url, function(file, err)
+    createDownloadTaskAsync(url, function(file, err)
         if err ~= nil then
             m.logger.e('Error downloading image', err)
             return
@@ -143,7 +195,7 @@ function m:setRandomDesktopPhoto()
 end
 
 function m:_onSpaceChanged()
-    m.logger.d('refreshingBackground', m.lastBackgroundFile)
+    m.logger.d('_onSpaceChanged:refreshingBackground', m.lastBackgroundFile)
     if m.lastBackgroundFile ~= nil then
         m:_setBackground(m.lastBackgroundFile)
     end
